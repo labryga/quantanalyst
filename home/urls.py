86a69bfdc7bda4b70a986333c53311523ae62e8f
Martin Labryga <martin@labryga.ch>
2018-11-10 22:46:27 +0000

    deployment init step 01

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^$', Home.as_view()),
-    url(r'^grid$', Grid.as_view())
+    url(r'^', Home.as_view()),
+    # url(r'^grid$', Grid.as_view())
 ]
--- home/views.py
+++ home/views.py
@@ -3,5 +3,3 @@ from django.views.generic import TemplateView
 class Home(TemplateView):
     template_name = "home.html"
 
-class Grid(TemplateView):
-    template_name = "grid.html"
