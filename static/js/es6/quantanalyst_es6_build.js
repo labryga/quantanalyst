3d415123d0547b817853b228e3bf204d77941693
Martin Labryga <martin@labryga.ch>
2018-11-13 17:45:01 +0100

    gulp reconfigured; pug files optimized

---
+++ static/js/es6/quantanalyst_es6_build.js
@@ -0,0 +1,15 @@
+
+class Navigation {
+	constructor(arg1, arg2) {
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+	}
+}
+
+
+class Slide {
+	constructor(arg1, arg2) {
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+	}
+}
---
+++ static/js/production/quantanalyst.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Navigation = function Navigation(arg1, arg2) {
+  _classCallCheck(this, Navigation);
+
+  this.arg1 = arg1;
+  this.arg2 = arg2;
+};
+
+var Slide = function Slide(arg1, arg2) {
+  _classCallCheck(this, Slide);
+
+  this.arg1 = arg1;
+  this.arg2 = arg2;
+};
