b37e24cfccdc1f87848b1c372a2bb3f5b747958f
Martin Labryga <martin@labryga.ch>
2018-11-20 16:12:58 +0100

    mobile navigation javascript toggle submenu display

--- staticfiles/js/production/quantanalyst.js
+++ staticfiles/js/production/quantanalyst.js
@@ -1,5 +1,13 @@
 "use strict";
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 navigation.onclick = function (event) {
   var lists = navigation.getElementsByTagName('UL');
   [].forEach.call(lists, function (item) {
@@ -7,4 +15,21 @@ navigation.onclick = function (event) {
   });
   var list_title = event.target.dataset.navigationType;
   document.getElementById(list_title).style.display = "block";
+};
+
+navigation_mobile.onclick = function (event) {
+  var list_navigation_type = event.target.dataset.navigationType;
+
+  if (list_navigation_type === "menu_mobile") {
+    var lists_mobile_collection = navigation_mobile.getElementsByTagName('UL');
+
+    var lists_mobile = _toConsumableArray(lists_mobile_collection);
+
+    var lists_mobile_submenus = lists_mobile.slice(1);
+    lists_mobile_submenus.forEach(function (item) {
+      return item.style.display = "none";
+    });
+  } else {
+    document.getElementById(list_navigation_type).style.display = "block";
+  }
 };
