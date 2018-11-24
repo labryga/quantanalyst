ec74f767ffa233846fc6af3c38c7c41b70093c9b
Martin Labryga <martin@labryga.ch>
2018-11-24 05:07:15 +0100

    tinymce installed

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import BlogEintrag
 
-# Register your models here.
+
+class BlogEintragAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(BlogEintrag, BlogEintragAdmin)
