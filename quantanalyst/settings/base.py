37f6419af51eb08aa5af379c326a7814b7d672c7
Martin Labryga <martin@labryga.ch>
2018-11-24 04:34:09 +0100

    staticfiles url updated

--- quantanalyst/settings/base.py
+++ quantanalyst/settings/base.py
@@ -97,7 +97,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILE_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
