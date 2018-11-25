47a738b9992d99cc2548baaa6323af7112242ede
Martin Labryga <martin@labryga.ch>
2018-11-25 16:15:47 +0100

    timymce tables (relations) dropped

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from . models import BlogEintrag
 
 
 class BlogEintragAdmin(admin.ModelAdmin):
--- home/models.py
+++ home/models.py
@@ -1,6 +1,4 @@
 from django.db import models
 
 
-class BlogEintrag(models.Model):
-    titel = models.TextField()
 
