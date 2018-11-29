ab1dcdb3a887d703296b1972d04e3f9a78e34441
Martin Labryga <martin@labryga.ch>
2018-11-29 04:21:52 +0100

    testing with 3d.js

--- blogpython/urls.py
+++ blogpython/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<slug>[-\w]+)', views.BlogEintrag.as_view(), name='blog-eintrag'),
+    url(r'^(?P<slug>[-\w]+)$', views.BlogEintrag.as_view(), name='blog-eintrag'),
 ]
