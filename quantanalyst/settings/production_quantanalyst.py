b56eea7fc6ca922126940747ce0fba622cd85f3c
Martin Labryga <martin@labryga.ch>
2018-12-04 04:51:39 +0000

    Csv imported to production settings from python decouple

--- quantanalyst/settings/production_quantanalyst.py
+++ quantanalyst/settings/production_quantanalyst.py
@@ -1,9 +1,10 @@
 import os
-from decouple import config
+from decouple import config, Csv
 from . base import *
 
 
 DEBUG = True
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS_PRODUCTION_QUANTANALYST')
+# ALLOWED_HOSTS = ['quantanalyst.ch', 'www.quantanalyst.ch']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS_PRODUCTION_QUANTANALYST', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS_PRODUCTION_QUANTANALYST')
 
