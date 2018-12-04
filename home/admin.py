d462484b4da9b9530e15fdcc234276035c43495e
Martin Labryga <martin@labryga.ch>
2018-12-04 03:40:00 +0100

    project_templates initialized for code refactoring

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class BlogEntryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.HomeBlog, BlogEntryAdmin)
--- home/models.py
+++ home/models.py
@@ -1,5 +1,11 @@
+import sys
+sys.path.append("..")
+
+from project_templates.models import BlogEntry
 from django.db import models
 from ckeditor.fields import RichTextField
 from ckeditor.widgets import CKEditorWidget
 
 
+class HomeBlog(BlogEntry):
+    pass
