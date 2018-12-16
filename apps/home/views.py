55f809d39fc8276ed925f1754a4caa16cdf20bc4
Martin Labryga <martin@labryga.ch>
2018-12-16 23:48:44 +0100

    home view mit get_context_data extended

--- apps/home/views.py
+++ apps/home/views.py
@@ -9,3 +9,16 @@ class Home(ListView):
     template_name = "base.pug"
     queryset = models.HomeMenuItems.objects.filter(active=True).order_by('order')
     context_object_name = "menu_list"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ModellsimulationBlogPost, self).get_context_data(*args, **kwargs)
+        context["menu_list"] = HomeMenuItems.objects.filter(active=True).order_by('order')
+        return context
+
+    def get_template_names(self):
+        if self.template_name is None:
+            raise ImproperlyConfigured(
+        else:
+            if self.request.is_ajax():
+                self.template_name = "BlogEintragAjax.pug"
+            return [self.template_name]
