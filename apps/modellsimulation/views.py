45b244e9ac160efcff7a6a2f155b9bc305ae90fc
Martin Labryga <martin@labryga.ch>
2018-12-17 21:46:01 +0100

    init ajax adjustment

--- apps/modellsimulation/views.py
+++ apps/modellsimulation/views.py
@@ -4,23 +4,24 @@ from home.models import HomeMenuItems
 
 
 class ModellsimulationBlogEntries(ListView):
-    template_name = "BlogEintragList.pug"
     model = models.ModellsimulationBlog
+
 
 class ModellsimulationBlogPost(DetailView):
     template_name = "BlogEintrag.pug"
     model = models.ModellsimulationBlog
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(ModellsimulationBlogPost, self).get_context_data(*args, **kwargs)
-        context["menu_list"] = HomeMenuItems.objects.filter(active=True).order_by('order')
-        return context
-
     def get_template_names(self):
-        if self.template_name is None:
-            raise ImproperlyConfigured(
-        else:
-            if self.request.is_ajax():
-                self.template_name = "BlogEintragAjax.pug"
-            return [self.template_name]
+        names = super(ModellsimulationBlogPost, self).get_template_names()
+        if self.request.is_ajax():
+            template_name = "BlogEintragAjax.pug"
+            names = [self.template_name]
+        return names
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ModellsimulationBlogPost, self).get_context_data(*args, **kwargs)
+    #     if not self.request.is_ajax():
+    #         context["menu_list"] = HomeMenuItems.objects.filter(active=True).order_by('order')
+    #     return context
+
 
