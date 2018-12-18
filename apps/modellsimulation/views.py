663684dd890e4d5edb3422f0902412fb96b99bac
Martin Labryga <martin@labryga.ch>
2018-12-18 23:25:47 +0100

    head template refactored

--- apps/modellsimulation/views.py
+++ apps/modellsimulation/views.py
@@ -1,24 +1,12 @@
 import sys
 sys.path.append("..")
-from project_templates.views import BlogPost
+from project_templates.views import BlogPost, BlogEntries
 
-from django.views.generic import ListView, DetailView
 from . import models
-from home.models import HomeMenuItems
 
 
-class ModellsimulationBlogEntries(ListView):
+class ModellsimulationBlogEntries(BlogEntries):
     model = models.ModellsimulationBlog
-    def get_context_data(self, **kwargs):
-        context = super(ModellsimulationBlogEntries, self).get_context_data(**kwargs)
-        if not self.request.is_ajax():
-            context["menu_list"] = HomeMenuItems.objects.filter(active=True).order_by('order')
-        return context
-
-    def get_template_names(self):
-        self.template_name = "BlogEintragList.pug" if self.request.is_ajax() \
-                else "base_test.pug"
-        return [self.template_name]
 
 
 class ModellsimulationBlogPost(BlogPost):
--- project_templates/views.py
+++ project_templates/views.py
@@ -1,13 +1,25 @@
 from home.models import HomeMenuItems
 
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 
 class BlogPost(DetailView):
 
     def get_template_names(self):
-        self.template_name = "BlogEintragAjax.pug" \
-                if self.request.is_ajax() else "BlogEintrag.pug"
+        self.template_name = "BlogEintragAjax.pug" if self.request.is_ajax() \
+                else "BlogEintrag.pug"
         return [self.template_name]
 
 
+class BlogEntries(ListView):
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogEntries, self).get_context_data(**kwargs)
+        if not self.request.is_ajax():
+            context["menu_list"] = HomeMenuItems.objects.filter(active=True).order_by('order')
+        return context
+
+    def get_template_names(self):
+        self.template_name = "BlogEintragList.pug" if self.request.is_ajax() \
+                else "base.pug"
+        return [self.template_name]
