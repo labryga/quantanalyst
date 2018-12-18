1349fd09fc5bb2fa589909d942862f9feca4b322
Martin Labryga <martin@labryga.ch>
2018-12-18 01:01:55 +0100

    project_template views line break added

--- apps/modellsimulation/views.py
+++ apps/modellsimulation/views.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 from project_templates.views import BlogPost
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from . import models
 from home.models import HomeMenuItems
 
--- project_templates/views.py
+++ project_templates/views.py
@@ -1,8 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView
 
 
 class BlogPost(DetailView):
 
     def get_template_names(self):
-        self.template_name = "BlogEintragAjax.pug" if self.request.is_ajax() else "BlogEintrag.pug"
+        self.template_name = "BlogEintragAjax.pug" \
+                if self.request.is_ajax() else "BlogEintrag.pug"
         return [self.template_name]
