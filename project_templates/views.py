7961ad2c6874ac6e002dfb05a111fa6220e992e3
Martin Labryga <martin@labryga.ch>
2018-12-19 00:33:04 +0100

    ajax not-ajax views and templates extendedł

--- project_templates/views.py
+++ project_templates/views.py
@@ -13,15 +13,20 @@ class BlogEntries(ListView):
 
     def get_template_names(self):
         self.template_name = "BlogEintragList.pug" if self.request.is_ajax() \
-                else "base.pug"
+                else "ext_list.pug"
         return [self.template_name]
 
 
 class BlogPost(DetailView):
 
+    def get_context_data(self, **kwargs):
+        context = super(BlogPost, self).get_context_data(**kwargs)
+        context["menu_list"] = HomeMenuItems.objects.filter(active=True).order_by('order')
+        return context
+
     def get_template_names(self):
         self.template_name = "BlogEintragAjax.pug" if self.request.is_ajax() \
-                else "BlogEintrag.pug"
+                else "ext_detail.pug"
         return [self.template_name]
 
 
