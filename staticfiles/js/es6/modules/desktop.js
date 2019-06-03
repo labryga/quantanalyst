7e3b1213e04d471ebb66a63d09effcf591a78b93
Martin Labryga <martin@labryga.ch>
2019-06-03 22:34:04 +0200

    indentation fixed in navigation.js

--- staticfiles/js/es6/modules/desktop.js
+++ staticfiles/js/es6/modules/desktop.js
@@ -1,3 +1,4 @@
+
 let desktop_tablet_xl_device = window.matchMedia("(min-width: 900px) and (max-width: 2500px)");
 get_deskotp_tablet_menu(desktop_tablet_xl_device);
 desktop_tablet_xl_device.addListener(get_deskotp_tablet_menu);
--- staticfiles/js/es6/quantanalyst_es6_build.js
+++ staticfiles/js/es6/quantanalyst_es6_build.js
@@ -1,3 +1,4 @@
+
 let desktop_tablet_xl_device = window.matchMedia("(min-width: 900px) and (max-width: 2500px)");
 get_deskotp_tablet_menu(desktop_tablet_xl_device);
 desktop_tablet_xl_device.addListener(get_deskotp_tablet_menu);
@@ -156,38 +157,38 @@ function get_deskotp_tablet_menu(desktop_tablet_xl_device) {
 
 [menu, inhalt].forEach(function(item) {
 		item.addEventListener("click", {
-				handleEvent(event) {
-						event.preventDefault(); 
-						if (event.target.id == "menu_phone") {
+			handleEvent(event) {
+				event.preventDefault(); 
+				if (event.target.id == "menu_phone") {
 
-								if (phone.style.display == "block") {
-									phone.style.display = "none";
-									phone.classList.remove("phone_list");
+						if (phone.style.display == "block") {
+							phone.style.display = "none";
+							phone.classList.remove("phone_list");
 
-								} else { 
-										phone.style.display = "block";
-										phone.classList.add("phone_list");
-								}
-
-						} else if (event.target.nodeName == "A" && event.target.closest("DIV").id == "menu" ) {
-								// crucial header settings for django "self.request.is_ajax()" returning "true"
-								let li_items = [...menu.getElementsByTagName("A")];
-								li_items.forEach(li_item => li_item.classList.remove("menu_item"));
-								event.target.classList.add("menu_item");
-								axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-								axios.get(event.target.pathname)
-									.then(function (response) {
-											inhalt.innerHTML = response.data;}); 
-						} else if (event.target.id == "menu_mobile") {
-								alert("mobile!");
-					
-						} else {
-								axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-								axios.get(event.target.pathname)
-									.then(function (response) {
-											inhalt.innerHTML = response.data;}); 
+						} else { 
+								phone.style.display = "block";
+								phone.classList.add("phone_list");
 						}
+
+				} else if (event.target.nodeName == "A" && event.target.closest("DIV").id == "menu" ) {
+						// crucial header settings for django "self.request.is_ajax()" returning "true"
+						let li_items = [...menu.getElementsByTagName("A")];
+						li_items.forEach(li_item => li_item.classList.remove("menu_item"));
+						event.target.classList.add("menu_item");
+						axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+						axios.get(event.target.pathname)
+							.then(function (response) {
+									inhalt.innerHTML = response.data;}); 
+				} else if (event.target.id == "menu_mobile") {
+						alert("mobile!");
+			
+				} else {
+						axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+						axios.get(event.target.pathname)
+							.then(function (response) {
+									inhalt.innerHTML = response.data;}); 
 				}
+			}
 		});
 });
 
